9b0da7b261e5c22d3018b0482e9af697535e4c6e
Carlos Gómez García <carlos@khasmlabs.com>
2024-12-05 10:15:14 -0600

    test.py and gitignore added

--- test.py
+++ test.py
@@ -1,14 +1,60 @@
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama.llms import OllamaLLM
+from langchain_core.tools import tool
+from langchain_ollama import ChatOllama
+from langchain_core.messages import HumanMessage
 
-template = """Question: {question}
+@tool
+def add(a: int, b: int) -> int:
+    """Sum two integer numbers
 
-Answer: Let's think step by step."""
+    Args:
+       a (int): the first number to sum
+       b (int): the second number to sum
 
-prompt = ChatPromptTemplate.from_template(template)
+    """
+    return a + b
 
-model = OllamaLLM(model="llama3.1", temperature=0)
+@tool
+def mul(a: float, b: float) -> float:
+    """Multiply two numbers
 
-chain = prompt | model
+    Args:
+        a (float): first number to multiply
+        b (float): second number to multiply
 
-chain.invoke({"question": "What is LangChain?"})
+    """
+    return a * b
+
+@tool
+def div(a: float, b: float) -> float:
+    """Divide two numbers
+
+    Args:
+        a (float): first number to divide
+        b (float): second number to divide
+
+    """
+    return a / b
+
+llm = ChatOllama(
+    model="llama3.1",
+    temperature=0,
+).bind_tools([add, mul, div])
+
+
+user_query = input("Please enter your query: ")
+
+
+messages = [HumanMessage(user_query)]
+
+ai_msg = llm.invoke(messages)
+
+print(ai_msg.tool_calls)
+
+messages.append(ai_msg)
+
+for tool_call in ai_msg.tool_calls:
+    selected_tool = {"add": add, "mul": mul, "div": div}[tool_call["name"].lower()]
+    result = selected_tool.invoke(tool_call)
+    messages.append(result)
+
+    print(f"\nResult of the operation: {tool_call['name']}: {result.content}")
